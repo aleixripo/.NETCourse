091d51560b6afdd881e9adacd15d3556a7c25b07
ALEIX RIPOLL SABATE <aleix.ripoll.ext@IBL.INETUM>
2022-10-07 11:39:34 +0200

    First Commit

---
+++ UniversityApiBackend/Models/DataModels/Course.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Course : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, MinLength(280)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string LongDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string TargetPublic { get; set; } = string.Empty;
+
+        [Required]
+        public string Targets { get; set; } = string.Empty;
+
+        [Required]
+        public string Requeriments { get; set; } = string.Empty;
+
+        public enum Level
+        {
+            Basic,
+            Medium,
+            Advanced,
+            Expert
+        }
+
+        public Level level { get; set; } = Level.Basic;
+    }
+}
